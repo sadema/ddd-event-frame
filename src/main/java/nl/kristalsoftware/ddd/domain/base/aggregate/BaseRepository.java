8109389f32706976b44cc53e7063b09e992a1381
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2023-02-28 19:57:01 +0100

    [develop] removed disabled method

--- src/main/java/nl/kristalsoftware/ddd/domain/base/aggregate/BaseRepository.java
+++ src/main/java/nl/kristalsoftware/ddd/domain/base/aggregate/BaseRepository.java
@@ -1,5 +1,4 @@
 package nl.kristalsoftware.ddd.domain.base.aggregate;
 
 public interface BaseRepository<R extends AggregateRepository, T extends BaseAggregateRoot> {
-//    List<DomainEventSaving<R>> sendCommand(BaseCommand<R,T> command);
 }
